da5cd6baa59adf26d28bab170b0f78233e106be9
adejolx <dejoladaniel@gmail.com>
2024-07-31 08:42:06 +0100

    feat: add some keymaps and options
    * scroll offset of 999 puts cursorline at middle of buffer during vertical navigation
    
    * add options for indentation
    
    * put cursorline at middle of buffer after navigating half a page (up or down)

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -4,3 +4,7 @@ opt.termguicolors = true
 opt.number = true
 opt.relativenumber = true
 opt.hlsearch = false
+opt.backspace = 'indent,eol,start'
+opt.autoindent = true
+opt.cursorline = true
+opt.scrolloff = 999
