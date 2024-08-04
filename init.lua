66bd17e13eaffde8de806cd414cd353d558a6831
adejolx <dejoladaniel@gmail.com>
2024-08-04 15:18:58 +0100

    feat: sort open buffers by most recently used
    Undid some keymap mods from a previous commit
    Formats lua config

--- init.lua
+++ init.lua
@@ -389,7 +389,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        builtin.buffers { sort_mru = true }
+      end, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -675,11 +677,11 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript =  { 'prettierd', 'prettier', stop_after_first = true  },
-        typescript = {  'prettierd', 'prettier', stop_after_first=true  },
-        typescriptreact =  { 'prettierd', 'prettier', stop_after_first  = true  },
-        javascriptreact = {  'prettierd', 'prettier', stop_after_first=true  },
-        css = {  'prettierd', 'prettier' , stop_after_first= true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
