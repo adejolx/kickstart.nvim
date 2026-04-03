cf32a373d5a67608623230d71d11e79555148737
adejolx <54010799+adejolx@users.noreply.github.com>
2026-04-03 14:52:34 +0100

    feat(config): add keymap for floating diagnostic messages
    map <leader>e to open_float to allow viewing full diagnostic details
    without jumping to the line. this improves visibility of lsp errors
    and warnings during navigation.

--- init.lua
+++ init.lua
@@ -191,6 +191,7 @@ vim.diagnostic.config {
   jump = { float = true },
 }
 
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
