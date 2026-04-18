914fe03589a8e6ced355e1c368c7e8bd01d1c1ac
adejolx <54010799+adejolx@users.noreply.github.com>
2026-04-18 09:06:01 +0100

    chore(ui): switch colorscheme to tokyonight
    Replaces makurai theme with tokyonight for a cleaner and more
    maintained default appearance.

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,13 +1,7 @@
 return {
-  'Skardyy/makurai-nvim',
-  config = function()
-    -- you don't have to call setup
-    require('makurai').setup {
-      increase_contrast = true,
-      bordered = true,
-      transparent = false,
-    }
-
-    vim.cmd.colorscheme 'makurai_dark'
-  end,
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function() vim.cmd [[colorscheme tokyonight]] end,
 }
