d3d69b9043ad965155180255a465c5e5fe5598a2
Daniel Oyebanjo <daniel@korapay.com>
2026-06-09 09:37:41 +0100

    feat(copilot): add explicit suggestion accept keymap

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
   'github/copilot.vim',
+  event = 'InsertEnter',
+
+  init = function() vim.g.copilot_no_tab_map = true end,
+
+  config = function()
+    vim.keymap.set('i', '<C-l>', 'copilot#Accept("")', {
+      expr = true,
+      replace_keycodes = false,
+      silent = true,
+      desc = 'Accept Copilot suggestion',
+    })
+  end,
 }
